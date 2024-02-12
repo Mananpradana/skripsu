6d4825d6fbaf96da0290926787b1ec09865e0187
woyman <woyman11@gmail.com>
2024-02-12 08:55:47 +0700

    add chart for pasien

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,12 +5,20 @@ import App from '../vue/component/AppMain'
 
 import { Icon } from 'leaflet';
 
+import HighchartsVue from 'highcharts-vue'
+import Highcharts from "highcharts";
+
 delete Icon.Default.prototype._getIconUrl;
 Icon.Default.mergeOptions({
   iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
   iconUrl: require('leaflet/dist/images/marker-icon.png'),
   shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
 });
+
+
+Vue.use(HighchartsVue, {
+  highcharts: Highcharts
+})
 
 const app = new Vue({
     el: '#app',
